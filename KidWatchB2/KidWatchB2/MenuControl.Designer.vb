ed8937951ebfa9c119ac0c8e0b1fc1c2f40f347b
mygitaccount1234 <45018871+mygitaccount1234@users.noreply.github.com>
2018-11-15 13:00:31 -0500

    added messages button

--- KidWatchB2/KidWatchB2/MenuControl.Designer.vb
+++ KidWatchB2/KidWatchB2/MenuControl.Designer.vb
@@ -24,8 +24,10 @@ Partial Class MenuControl
     Private Sub InitializeComponent()
         Me.mapsLabel = New System.Windows.Forms.Label()
         Me.walletLabel = New System.Windows.Forms.Label()
+        Me.TextButton = New System.Windows.Forms.Button()
         Me.mapsButton = New System.Windows.Forms.Button()
         Me.walletButton = New System.Windows.Forms.Button()
+        Me.MessagesLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'mapsLabel
@@ -52,6 +54,15 @@ Partial Class MenuControl
         Me.walletLabel.TabIndex = 6
         Me.walletLabel.Text = "Wallet"
         '
+        'TextButton
+        '
+        Me.TextButton.BackgroundImage = Global.KidWatchB2.My.Resources.Resources._1891029
+        Me.TextButton.Location = New System.Drawing.Point(32, 178)
+        Me.TextButton.Name = "TextButton"
+        Me.TextButton.Size = New System.Drawing.Size(49, 49)
+        Me.TextButton.TabIndex = 8
+        Me.TextButton.UseVisualStyleBackColor = True
+        '
         'mapsButton
         '
         Me.mapsButton.BackgroundImage = Global.KidWatchB2.My.Resources.Resources.Apps_Google_Maps_icon
@@ -70,11 +81,24 @@ Partial Class MenuControl
         Me.walletButton.TabIndex = 4
         Me.walletButton.UseVisualStyleBackColor = True
         '
+        'MessagesLabel
+        '
+        Me.MessagesLabel.AutoSize = True
+        Me.MessagesLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessagesLabel.ForeColor = System.Drawing.Color.Transparent
+        Me.MessagesLabel.Location = New System.Drawing.Point(19, 230)
+        Me.MessagesLabel.Name = "MessagesLabel"
+        Me.MessagesLabel.Size = New System.Drawing.Size(77, 18)
+        Me.MessagesLabel.TabIndex = 9
+        Me.MessagesLabel.Text = "Messages"
+        '
         'MenuControl
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
+        Me.Controls.Add(Me.MessagesLabel)
+        Me.Controls.Add(Me.TextButton)
         Me.Controls.Add(Me.mapsLabel)
         Me.Controls.Add(Me.walletLabel)
         Me.Controls.Add(Me.mapsButton)
@@ -90,4 +114,6 @@ Partial Class MenuControl
     Friend WithEvents walletLabel As Label
     Friend WithEvents mapsButton As Button
     Friend WithEvents walletButton As Button
+    Friend WithEvents TextButton As Button
+    Friend WithEvents MessagesLabel As Label
 End Class
